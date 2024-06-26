f209dec964f20d21f144d3b20b5f48287fc1cdef
CLOR2003 <lorchakong1@gmail.com>
2024-06-26 04:41:34 -0500

    Added an Error page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
-import Home from './Home.js'
+import Home from './Home'
+import Error from './Error'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path = "/Homework" element={<Home />} />
+        <Route path='/Homework/*' element={<Error/>} />
       </Routes>
     </Router>
   );
