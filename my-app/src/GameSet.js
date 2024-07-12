aeb6e746381e7a2dc5b1c616a8a0dee82bc92801
CLOR2003 <lorchakong1@gmail.com>
2024-07-12 17:20:13 -0500

    Worked on new controls and integrating to default controls

--- my-app/src/GameSet.js
+++ my-app/src/GameSet.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { Scene, Engine, FreeCamera, Vector3, HemisphericLight, MeshBuilder } from "@babylonjs/core";
+import { Scene, Engine, UniversalCamera, Vector3, HemisphericLight, MeshBuilder,Axis } from "@babylonjs/core";
 import testControls from "./controls";
 
 export default function Game(){
@@ -16,13 +16,15 @@ export default function Game(){
     const scene = new Scene(engine);
 
     // This creates and positions a free camera (non-mesh)
-    const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+    const camera = new UniversalCamera("UniversalCamera", new Vector3(0, 5, -10), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(Vector3.Zero());
 
     // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+    camera.attachControl(canvas,true);
+
+    camera.speed = 0.2;
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
@@ -40,21 +42,29 @@ export default function Game(){
     MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 
     var inputMap=testControls(scene);
-
+    
     scene.onBeforeRenderObservable.add(() => {
-      if (inputMap["w"] || inputMap["ArrowUp"]) {
-          camera.position.z += 0.1;
+
+
+      if (inputMap["w"]) {
+        camera.position.addInPlace(camera.getDirection(Axis.Z).scale(0.1));
       }
-      if (inputMap["s"] || inputMap["ArrowDown"]) {
-          camera.position.z -= 0.1;
+      if (inputMap["s"]) {
+        camera.position.addInPlace(camera.getDirection(Axis.Z).scale(-0.1));
       }
-      if (inputMap["a"] || inputMap["ArrowLeft"]) {
-          camera.position.x -= 0.1;
+      if (inputMap["a"]) {
+        camera.position.addInPlace(camera.getDirection(Axis.X).scale(-0.1));
       }
-      if (inputMap["d"] || inputMap["ArrowRight"]) {
-          camera.position.x += 0.1;
+      if (inputMap["d"]) {
+        camera.position.addInPlace(camera.getDirection(Axis.X).scale(0.1));
       }
-  });
+      if (inputMap[" "]){
+        camera.position.y += 0.1;
+      }
+      if (inputMap["Shift"]){
+        camera.position.y -= 0.1;
+      }
+    });
 
   engine.runRenderLoop(() => {
     box.rotation.x += 0.01;
